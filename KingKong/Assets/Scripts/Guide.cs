f2160216a04639eea11ba16e6b9d4c517af5e312
KiemVM <manhkiem41@gmail.com>
2015-11-15 09:32:57 +0700

    Thêm phần bắt đầu game

---
+++ KingKong/Assets/Scripts/Guide.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Guide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ClickBtnStart()
+    {
+        Application.LoadLevel("Start");
+    }
+}
---
+++ KingKong/Assets/Scripts/Scene1.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Scene1 : MonoBehaviour {
+
+	// Use this for initialization
+    private bool bPause = false;
+    public Button BtnPause;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ClickBtnStart()
+    {
+        Application.LoadLevel("Start");
+
+    }
+
+    public void clickBtnPause()
+    {
+        if (!bPause)
+        {
+            Time.timeScale = 0;
+            bPause = true;
+            Text text = BtnPause.GetComponentInChildren<Text>();
+            text.text = "Resume";
+        }
+          
+        else
+        {
+            Time.timeScale = 1;
+            bPause = false;
+            Text text = BtnPause.GetComponentInChildren<Text>();
+            text.text = "Pause";
+        }
+    }
+}
